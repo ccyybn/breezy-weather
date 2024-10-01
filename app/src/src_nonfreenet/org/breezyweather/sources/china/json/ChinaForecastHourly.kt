4d6d3fea114719ebfc1af03d8a063282b37dd0cc
cc <ccyybn@gmail.com>
2024-10-01 20:20:22 +0800

    Convert the Chinese AQI index into pm2.5 (μg/m3) and then adapt it to the US standard AQI

--- app/src/src_nonfreenet/org/breezyweather/sources/china/json/ChinaForecastHourly.kt
+++ app/src/src_nonfreenet/org/breezyweather/sources/china/json/ChinaForecastHourly.kt
@@ -23,5 +23,6 @@ data class ChinaForecastHourly(
     val desc: String?,
     val temperature: ChinaValueListInt?,
     val weather: ChinaValueListInt?,
+    val aqi: ChinaValueListInt?,
     val wind: ChinaHourlyWind?
 )
