97dcc92598d6a8fbf69511aef8fa269d0c710b5f
cc <ccyybn@gmail.com>
2024-10-03 17:40:15 +0800

    Remove redundant minutely request within China source

--- app/src/src_nonfreenet/org/breezyweather/sources/china/json/ChinaForecastMinutely.kt
+++ app/src/src_nonfreenet/org/breezyweather/sources/china/json/ChinaForecastMinutely.kt
@@ -19,13 +19,7 @@ package org.breezyweather.sources.china.json
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ChinaForecastResult(
-    val current: ChinaCurrent? = null,
-    val forecastDaily: ChinaForecastDaily? = null,
-    val forecastHourly: ChinaForecastHourly? = null,
-    val minutely: ChinaForecastMinutely? = null,
-    val yesterday: ChinaYesterday? = null,
-    val updateTime: Long? = null,
-    val aqi: ChinaAqi? = null,
-    val alerts: List<ChinaAlert>? = null
+data class ChinaForecastMinutely(
+    val status: Int?,
+    val precipitation: ChinaMinutelyPrecipitation?
 )
